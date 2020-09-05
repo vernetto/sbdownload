1cb947869fc3b82df5c96c1463bae08f91c66241
vernetto <Ernesto0z>
2020-09-05 19:22:53 +0200

    adding

---
+++ src/test/java/org/pierre/sbdownload/SbdownloadApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pierre.sbdownload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbdownloadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
